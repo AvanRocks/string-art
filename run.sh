4925fd06b798287206128b15d5f4543aca034656
AvanRocks <64660241+AvanRocks@users.noreply.github.com>
2023-12-13 17:18:11 -0500

    added run.sh

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#./string-art -g -c tmp/house-cache -p 100 -i 100 -r 10 images/house.tiff -o tmp/house.png
+
+#./string-art -g -c tmp/test-cache-r-1 -p 100 -i 100 -r 1 images/test-768.png -o tmp/test-768.png
+#./string-art -g -c tmp/test-cache-r-5 -p 100 -i 100 -r 5 images/test-768.png -o tmp/test-768.png
+#./string-art -g -c tmp/test-cache-r-10 -p 100 -i 100 -r 10 images/test-768.png -o tmp/test-768.png
+#./string-art -g -c tmp/test-cache-r-15 -p 100 -i 100 -r 15 images/test-768.png -o tmp/test-768.png
+
+#./string-art -g -c tmp/test-cache-p-200-r-5 -p 200 -i 100 -r 10 images/test-768.png -o tmp/test-768.png
+
+#./string-art -c tmp/tasso-cache -p 200 -i 200 -r 10 images/tasso.jpg -o tmp/tasso.png
+./string-art -p 300 -i 4000 -r 10 images/tasso-1000.jpg -o tmp/tasso.png
+#./string-art -c tmp/tasso-1000-cache -p 300 -i 100 -r 10 images/tasso-1000.jpg -o tmp/tasso.png
+
+#./string-art -c tmp/tasso-cache -p 200 -i 100 -r 10 images/tasso-2.jpg -o tmp/tasso-2.png
