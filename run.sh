a869115d9048ed514d7fa233be5c233bc4c0f833
AvanRocks <64660241+AvanRocks@users.noreply.github.com>
2023-12-14 00:52:14 -0500

    update run.sh with string weight

--- run.sh
+++ run.sh
@@ -10,7 +10,8 @@
 #./string-art -g -c tmp/test-cache-p-200-r-5 -p 200 -i 100 -r 10 images/test-768.png -o tmp/test-768.png
 
 #./string-art -c tmp/tasso-cache -p 200 -i 200 -r 10 images/tasso.jpg -o tmp/tasso.png
-./string-art -p 300 -i 4000 -r 10 images/tasso-1000.jpg -o tmp/tasso.png
+#./string-art -p 300 -i 4000 -r 10 images/tasso-1000.jpg -o tmp/tasso.png
+./string-art -s 20 -p 288 -i 10000 -r 1 images/tasso-1000.jpg -o tmp/tasso.png
 #./string-art -c tmp/tasso-1000-cache -p 300 -i 100 -r 10 images/tasso-1000.jpg -o tmp/tasso.png
 
 #./string-art -c tmp/tasso-cache -p 200 -i 100 -r 10 images/tasso-2.jpg -o tmp/tasso-2.png
