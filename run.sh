28688eac3c98da5e00aadce021e6466cb2cf1ad1
AvanRocks <64660241+AvanRocks@users.noreply.github.com>
2023-12-18 17:36:44 -0500

    update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-./string-art -s 20 -p 288 -i 10000 -m 10 images/tasso-1000.jpg -o tmp/tasso.png
+#./string-art -stop -p 300 -l 20000 images/tasso-2-cropped.jpg -o tmp/tasso-2.png
+#./string-art -v -stop -p 300 -l 10000 images/tasso-2-cropped.jpg -o tmp/tasso-2-vid.mkv
+
+#./string-art -stop -p 300 -l 20000 images/house-1000.png -o tmp/house.png
+#./string-art -stop -p 300 -l 20000 images/man-face-1.jpg -o tmp/man-face.png
+#./string-art -stop -p 300 -l 20000 images/peppers-1000.tiff -o tmp/peppers.tiff
+#./string-art -stop -rgb -p 300 -l 20000 images/peppers-1000.tiff -o tmp/peppers-rgb.tiff
+./string-art -v -stop -p 300 -l 20000 images/man-face-1.jpg -o tmp/man-face-vid.mkv
